da61482681d160f1da0be36f4da3b38fa42ba8d5
YJ Soon <yjsoon@tinkertanker.com>
2018-09-05 15:45:49 +0800

    Few more UI fixes

---
+++ TasksOnFire/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  TasksOnFire
+//
+//  Created by Soon Yin Jie on 5/9/18.
+//  Copyright © 2018 Tinkertanker. All rights reserved.
+//
+
+import Foundation
+
+struct Task {
+    
+    var name: String
+    var completed: Bool
+    var addedByUser: String 
+    
+}
---
+++ TasksOnFire/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  TasksOnFire
+//
+//  Created by Soon Yin Jie on 5/9/18.
+//  Copyright © 2018 Tinkertanker. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    
+    var id: String
+    var email: String 
+    
+}
