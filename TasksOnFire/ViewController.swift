449091933ecb658a2d9b2cc1d895ae0b9f6bc878
YJ Soon <yjsoon@tinkertanker.com>
2018-09-05 14:20:52 +0800

    Initial Commit

---
+++ TasksOnFire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TasksOnFire
+//
+//  Created by Soon Yin Jie on 5/9/18.
+//  Copyright © 2018 Tinkertanker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
