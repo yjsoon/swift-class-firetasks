7a02c63a62effbdcefa404d2093fd58e92e848fa
YJ Soon <yjsoon@tinkertanker.com>
2018-09-06 10:52:24 +0800

    Users I think?

--- TasksOnFire/ViewController.swift
+++ TasksOnFire/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func signupButtonPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Sign up", message: "", preferredStyle: .alert)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Email address"
+        }
+        alert.addTextField { (textField) in
+            textField.placeholder = "Password"
+            textField.isSecureTextEntry = true
+        }
+        
+        let signupAction = UIAlertAction(title: "Sign up", style: .default) { (_) in
+            let emailField = alert.textFields![0]
+            let passwordField = alert.textFields![1]
+            
+            if let email = emailField.text, let password = passwordField.text {
+                Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                    if (error == nil) {
+                        Auth.auth().signIn(withEmail: email, password: password, completion: nil)
+                    }
+                })
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(signupAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
 
